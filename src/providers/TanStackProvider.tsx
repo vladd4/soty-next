d510e19e4c68752637fd61093b3b685329550ba5
Vladyslav D <donetsvlad444@gmail.com>
2025-02-10 12:00:33 +0200

    remove dev tools

--- src/providers/TanStackProvider.tsx
+++ src/providers/TanStackProvider.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import { ReactNode, useState } from "react";
 
 export default function TanStackProvider({
@@ -11,9 +11,6 @@ export default function TanStackProvider({
 }) {
   const [queryClient] = useState(() => new QueryClient());
   return (
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
-      {children}
-    </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 }
